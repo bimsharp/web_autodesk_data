ccbcccf98b9d40ba3e991f1e8ba41fb30cce0727
bimsharp365 <bimsharp365@gmail.com>
2024-10-05 09:41:34 -0400

    - index - added middleware to wrap requests and responses with acces control headers

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,16 @@ routesFiles.forEach(function (file) {
     //debug(fullPath); //@ToDo: implement debug as devDependency when debugger attachment is more helpful
 });
 
+//@ts-ignore
+app.use(function(req, res) {
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  });
+
 app.listen(process.env.PORT, () => {
     console.log(`app listening on port ${process.env.PORT}`)
 });
