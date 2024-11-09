babbd262042cc582434288ddaf4e0b6ce3e544ab
bimsharp365 <bimsharp365@gmail.com>
2024-11-09 18:53:55 -0500

    fixed headers for app.get('/autodesk-authentication/three-legged

--- src/autodesk-authentication/routes/postV2TokenThreeLegged.ts
+++ src/autodesk-authentication/routes/postV2TokenThreeLegged.ts
@@ -8,6 +8,14 @@ import { Request, Response } from 'express';
 module.exports = function (app) {
 
     app.get('/autodesk-authentication/three-legged', async (req: Request, res: Response) => {
+        //@ToDo: use debug package for this
+        //@ToDo: implement logger as a middleware
+        console.log(`GET /autodesk-authentication/three-legged`);
+        
+        //@ToDo: use middleware for this
+        res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Access-Control-Allow-Origin");
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
 
         const params: models.PostAutodeskTokenThreeLegged_Payload = {
             authorization_code: <string>req?.query?.code ?? ''
